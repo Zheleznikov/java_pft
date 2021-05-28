9c19a172eb9a83dc2ad48044a83ea3fcf7fd1a21
Serg <polwen@yandex.ru>
2021-05-28 17:41:26 +0300

    '1'

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -2,6 +2,11 @@ package ru.stqa.pft.sandbox;
 
 class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        int a = 2;
+        int b = 3;
+        String str = new String("He");
+        System.out.println("2");
     }
 }
