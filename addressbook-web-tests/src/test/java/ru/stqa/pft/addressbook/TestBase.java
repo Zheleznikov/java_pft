035638e8b3341c7a6e3c8e2d29199586d99765c6
Serg <polwen@yandex.ru>
2021-06-06 13:00:36 +0300

    Refactor push methods up to  TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -75,4 +75,12 @@ public class TestBase {
             return false;
         }
     }
+
+    protected void deleteSelectedGroups() {
+        wd.findElement(By.name("delete")).click();
+    }
+
+    protected void selectGroup() {
+        wd.findElement(By.name("selected[]")).click();
+    }
 }
