ec54db99ae8178e1739b6f8a4145549dcb030c6e
Serg <polwen@yandex.ru>
2021-06-06 12:23:27 +0300

    Add fillContactForm as method

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -20,25 +20,22 @@ public class ContactCreationTests {
 
     @Test
     public void testContactCreationTests() throws Exception {
-        wd.findElement(By.linkText("add new")).click();
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Cat");
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Cat");
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("theform")).click();
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("892");
-        wd.findElement(By.name("theform")).click();
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("cat@cat.cat");
+        gotoAddNewUserPage();
+        fillContactForm(new ContactData("Unique", "New-User-Original", "+1430555555", "unique@gmail.com"));
         wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
         wd.findElement(By.linkText("home")).click();
         wd.findElement(By.linkText("Logout")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    private void gotoAddNewUserPage() {
+        wd.findElement(By.linkText("add new")).click();
     }
 
     @AfterMethod(alwaysRun = true)
