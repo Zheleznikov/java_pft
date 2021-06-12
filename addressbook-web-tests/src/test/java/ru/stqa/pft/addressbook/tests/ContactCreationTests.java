2b2d42ca985573fd2852986d6a68d13947ba841f
Serg <polwen@yandex.ru>
2021-06-12 19:05:30 +0300

    Modificate methods - contactDeletion and ContactCreation with new methods in helpers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,8 +8,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() throws Exception {
         app.getNavigationHelper().gotoAddNewUserPage();
-        app.getContactHelper().fillContactForm(new ContactData("Unique", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts"), true);
-        app.getContactHelper().submitContactCreation();
+        app.getContactHelper().createContact(new ContactData("Unique", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts"));
         app.getNavigationHelper().gotoHomePage();
         app.getSessionHelper().logout();
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,11 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
     @Test
     public void testContactDeletion() {
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getNavigationHelper().gotoAddNewUserPage();
+            app.getContactHelper().createContact(new ContactData("contact for deleting", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts"));
+        }
+        app.getNavigationHelper().gotoHomePage();
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectedContacts();
         app.getContactHelper().acceptAlert();
