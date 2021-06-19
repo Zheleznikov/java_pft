197fb2e3f936b034da467f8117ef34e4a358834e
Serg <polwen@yandex.ru>
2021-06-19 17:41:59 +0300

    Add assertions to ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,18 +1,35 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() throws Exception {
-        ContactData contact = new ContactData("Unique", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts");
+        ContactData contact = new ContactData("new contact", "new contact", "+1430555555", "unique@gmail.com", "group for test contacts");
+
+        List<ContactData> before = app.getContactHelper().getContactList();
+
         app.getNavigationHelper().gotoAddNewUserPage();
         app.getContactHelper().createContact(contact);
         app.getNavigationHelper().gotoHomePage();
+
+        List<ContactData> after = app.getContactHelper().getContactList();
+
         app.getContactHelper().getContactList();
         app.getSessionHelper().logout();
+
+        Assert.assertEquals(before.size() + 1, after.size());
+        before.add(contact);
+        Comparator<? super ContactData> byId = (o1, o2) -> Integer.compare(o1.getId(), o1.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
 
