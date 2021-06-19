e65af3c400296fa040192acba9f75be051f0a2e4
Serg <polwen@yandex.ru>
2021-06-19 16:51:04 +0300

    Add assertion to ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
@@ -9,16 +13,24 @@ public class ContactModificationTests extends TestBase {
     public void testContactModification() {
         ContactData contactForCreation = new ContactData("contact for creating", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts");
         ContactData contactForModification = new ContactData("Modified contact", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts");
+
         if (!app.getContactHelper().isThereAContact()) {
             app.getNavigationHelper().gotoAddNewUserPage();
             app.getContactHelper().createContact(contactForCreation);
             app.getNavigationHelper().gotoHomePage();
         }
+        List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().selectContact();
         app.getContactHelper().modificateSelectedContacts();
         app.getContactHelper().fillContactForm(contactForModification, false);
         app.getContactHelper().submitContactModification();
         app.getNavigationHelper().gotoHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
         app.getSessionHelper().logout();
+
+        Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
