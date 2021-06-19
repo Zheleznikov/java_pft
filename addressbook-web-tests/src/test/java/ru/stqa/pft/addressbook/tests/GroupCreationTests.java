3b1ac252d7b8e8e5106db18cf1562c30a53663c9
Serg <polwen@yandex.ru>
2021-06-19 13:06:46 +0300

    Add comparator to sort lists of GroupDatas by Id

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -21,10 +21,11 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         app.getSessionHelper().logout();
         Assert.assertEquals(after.size(), before.size() + 1);
-        int max = after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId();
-        group.setId(max);
         before.add(group);
-        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+        Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 
