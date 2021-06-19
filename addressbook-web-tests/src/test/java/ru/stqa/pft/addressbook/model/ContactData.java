68a0e041d4da319b07c241535bdaa36527756a33
Serg <polwen@yandex.ru>
2021-06-19 16:23:45 +0300

    Modificate class ContactData - add field id and methods: toString, equals, hashCode

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,13 +1,26 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
+    private int id;
     private final String name;
     private final String lastName;
     private final String mobilePhone;
     private final String email;
     private final String group;
 
+    public ContactData(int id, String name, String lastName, String mobilePhone, String email, String group) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.mobilePhone = mobilePhone;
+        this.email = email;
+        this.group = group;
+    }
+
     public ContactData(String name, String lastName, String mobilePhone, String email, String group) {
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.lastName = lastName;
         this.mobilePhone = mobilePhone;
@@ -32,4 +45,26 @@ public class ContactData {
     }
 
     public String getGroup() {return group;}
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return Objects.equals(name, that.name) && Objects.equals(lastName, that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
+    }
 }
