6603ca0c563f374e577ce928cec41e754c5f19b1
Serg <polwen@yandex.ru>
2021-06-19 12:49:44 +0300

    Add lambda expression to get max id and to add it in list "before"

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -11,15 +13,18 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
+        GroupData group = new GroupData("some group2", "some header2", "some footer2");
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-//        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().createGroup(new GroupData("some group", "some header", "some footer"));
+        app.getGroupHelper().createGroup(group);
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
-//        int after = app.getGroupHelper().getGroupCount();
         app.getSessionHelper().logout();
         Assert.assertEquals(after.size(), before.size() + 1);
+        int max = after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId();
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
 
 
