cf2bb08d1d6ae0f9675c9fc218c1775d18a83f69
Serg <polwen@yandex.ru>
2021-06-25 19:40:31 +0300

    Add preconditions in delete and modify method

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,19 +10,25 @@ import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
-    @Test
-    public void testContactDeletion() {
-        ContactData contact = new ContactData("contact for deleting", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts");
+    @BeforeMethod
+    public void ensurePrecondition() {
+        app.goTo().gotoAddNewUserPage();
 
         if (!app.getContactHelper().isThereAContact()) {
+            ContactData contact = new ContactData("contact for deleting", "New-User-Original", "+1430555555", "unique@gmail.com", "group for test contacts");
             app.goTo().gotoAddNewUserPage();
             app.getContactHelper().createContact(contact);
             app.goTo().gotoHomePage();
         }
+    }
+
+    @Test
+    public void testContactDeletion() {
 
         List<ContactData> before = app.getContactHelper().getContactList();
+        int index = before.size() - 1;
 
-        app.getContactHelper().selectContact(before.size() - 1);
+        app.getContactHelper().selectContact(index);
         app.getContactHelper().deleteSelectedContacts();
         app.getContactHelper().acceptAlert();
         app.goTo().gotoHomePage();
@@ -28,7 +36,7 @@ public class ContactDeletionTests extends TestBase {
         List<ContactData> after = app.getContactHelper().getContactList();
 
         Assert.assertEquals(before.size() - 1, after.size());
-        before.remove(before.size() - 1);
+        before.remove(index);
         Assert.assertEquals(before, after);
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -9,28 +10,29 @@ import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
-    @Test
-    public void testContactModification() {
-        ContactData contactForCreation = new ContactData("Contact for modify", "Contact for modify", "+1430555555", "unique@gmail.com", "group for test contacts");
-        ContactData contactForModification = new ContactData("Modified contact", "Modified contact", "+1430555555", "unique@gmail.com", "group for test contacts");
-
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.goTo().gotoHomePage();
         if (!app.getContactHelper().isThereAContact()) {
+            ContactData contactForCreation = new ContactData("Contact for modify", "Contact for modify", "+1430555555", "unique@gmail.com", "group for test contacts");
             app.goTo().gotoAddNewUserPage();
             app.getContactHelper().createContact(contactForCreation);
             app.goTo().gotoHomePage();
         }
 
+    }
+
+    @Test
+    public void testContactModification() {
+        ContactData contactForModification = new ContactData("Modified contact", "Modified contact", "+1430555555", "unique@gmail.com", "group for test contacts");
         List<ContactData> before = app.getContactHelper().getContactList();
+        int index = before.size() - 1;
 
-        app.getContactHelper().modificateSelectedContacts(before.size() - 1);
-        app.getContactHelper().fillContactForm(contactForModification, false);
-        app.getContactHelper().submitContactModification();
+        app.getContactHelper().modify(contactForModification, index);
         app.goTo().gotoHomePage();
-
         List<ContactData> after = app.getContactHelper().getContactList();
-
         Assert.assertEquals(before.size(), after.size());
-        before.remove(before.size() - 1);
+        before.remove(index);
         before.add(contactForModification);
 
         Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
@@ -38,4 +40,5 @@ public class ContactModificationTests extends TestBase {
         after.sort(byId);
         Assert.assertEquals(before, after);
     }
+
 }
