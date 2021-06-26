52912c764600f068f944cb670b1dba15a26be0c9
Serg <polwen@yandex.ru>
2021-06-26 10:24:18 +0300

    Add getting editIcon in ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -32,7 +31,6 @@ public class ContactHelper extends HelperBase {
         } else {
             Assert.assertFalse(isElementPresent(By.name("new_group")));
         }
-
     }
 
     public void selectContact(int index) {
@@ -43,6 +41,10 @@ public class ContactHelper extends HelperBase {
         wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
     }
 
+    public void clickToModify(WebElement editIcon) {
+        editIcon.click();
+    }
+
     public void deleteSelectedContacts() {
         click(By.xpath("//input[@value='Delete']"));
     }
@@ -50,6 +52,7 @@ public class ContactHelper extends HelperBase {
 
     public void modifySelectedContacts(int index) {
         wd.findElements(By.xpath("//img[@alt='Edit']")).get(index).click();
+
     }
 
     public void submitModification() {
@@ -62,8 +65,8 @@ public class ContactHelper extends HelperBase {
     }
 
 
-    public void modify(ContactData contactForModification, int index) {
-        modifySelectedContacts(index);
+    public void modify(ContactData contactForModification) {
+        clickToModify(contactForModification.getEditIcon());
         fillContactForm(contactForModification, false);
         submitModification();
     }
@@ -72,34 +75,20 @@ public class ContactHelper extends HelperBase {
         return isElementPresent(By.name("selected[]"));
     }
 
-    public List<ContactData> list() {
-        List<ContactData> contacts = new ArrayList<>();
-        List<WebElement> elements = wd.findElements(By.xpath("//table[@id='maintable']//tr[@name='entry']"));
-        elements.forEach(el-> {
-            String lastName = el.findElement(By.xpath("./td[2]")).getText();
-            String name = el.findElement(By.xpath("./td[3]")).getText();
-            int id = Integer.parseInt(el.findElement(By.xpath("./td[1]/input")).getAttribute("id"));
-            ContactData contact = new ContactData()
-                    .withId(id)
-                    .withName(name)
-                    .withLastName(lastName);
-            contacts.add(contact);
-        });
-
-        return contacts;
-    }
-
-    public Set <ContactData> getAll() {
+    public Set<ContactData> getAll() {
         Set<ContactData> contacts = new HashSet<>();
         List<WebElement> elements = wd.findElements(By.xpath("//table[@id='maintable']//tr[@name='entry']"));
-        elements.forEach(el-> {
+        elements.forEach(el -> {
             String lastName = el.findElement(By.xpath("./td[2]")).getText();
             String name = el.findElement(By.xpath("./td[3]")).getText();
             int id = Integer.parseInt(el.findElement(By.xpath("./td[1]/input")).getAttribute("id"));
+            WebElement editIcon = el.findElement(By.xpath(".//img[@alt='Edit']"));
+
             ContactData contact = new ContactData()
                     .withId(id)
                     .withName(name)
-                    .withLastName(lastName);
+                    .withLastName(lastName)
+                    .withEditIcon(editIcon);
             contacts.add(contact);
         });
 
