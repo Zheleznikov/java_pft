170a5a33d10c2e466c8e44202659dc3150ec2ca4
Serg <polwen@yandex.ru>
2021-06-27 14:13:59 +0300

    Add precondition in contactPhoneTesting

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -10,6 +11,24 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTest extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.goTo().homePage();
+
+        if (app.contact().getAll().size() == 0) {
+            ContactData contact = new ContactData()
+                    .withName("name - phones Test")
+                    .withLastName("last name - phones Test")
+                    .withGroup("group for test contacts")
+                    .withMobilePhone("+7 (900) 12")
+                    .withWorkPhone("211-56-83")
+                    .withHomePhone("880050");
+            app.goTo().addNewUserPage();
+            app.contact().create(contact);
+            app.goTo().homePage();
+        }
+    }
 
     @Test
     public void testContactPhone() {
