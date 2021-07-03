7913d5bb6a1526990eeaef4ede92c0b92fbbc015
Serg <polwen@yandex.ru>
2021-07-03 13:25:41 +0300

    Use reloaded method attach to attach photo

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -29,7 +29,8 @@ public class ContactHelper extends HelperBase {
         type(By.name("email2"), contactData.getEmail2());
         type(By.name("email3"), contactData.getEmail3());
         type(By.name("address"), contactData.getCompanyAddress());
-        attach(By.name("photo"), contactData.getPhoto());
+//        attach(By.name("photo"), contactData.getPhoto());
+        attach(By.name("photo"), contactData.getPathToPhoto());
 
         if (creation) {
             new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
