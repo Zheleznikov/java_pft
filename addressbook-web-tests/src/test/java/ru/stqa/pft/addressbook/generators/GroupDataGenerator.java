87df7f85df75bfcbf406dde450cd03c9b6ffd1b7
Serg <polwen@yandex.ru>
2021-07-03 10:48:41 +0300

    Add json method

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -68,7 +70,6 @@ public class GroupDataGenerator {
                 e.printStackTrace();
             }
         });
-
 //        for (GroupData group : groups) {
 //            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
 //        }
@@ -76,7 +77,11 @@ public class GroupDataGenerator {
     }
 
     private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+//        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
         Writer writer = new FileWriter(file);
+        writer.write(json);
         writer.close();
     }
 
