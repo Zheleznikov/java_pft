146219a4f2456dd54fc0e50a0b5d3cb6c29344b4
Serg <polwen@yandex.ru>
2021-07-08 19:11:42 +0300

    Add logger

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import org.openqa.selenium.json.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -18,6 +20,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
 
     @DataProvider
     public Iterator<Object[]> basicValidGroups() {
@@ -75,7 +78,7 @@ public class GroupCreationTests extends TestBase {
     }
 
 
-    @Test(dataProvider = "xmlValidGroups")
+    @Test(dataProvider = "jsonValidGroups")
     public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         GroupSet before = app.group().getAll();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,25 +1,46 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
-public class TestBase {
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
-//    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+
+    //    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
+        System.out.println(System.getProperty("browser"));
         app.init();
     }
 
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method method, Object [] parameters) {
+        logger.info("Start test - " + method.getName() + " with parameters " + Arrays.asList(parameters));
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void togTestStop(Method method) {
+        logger.info("Stop test - " + method.getName());
+
     }
 
 }
