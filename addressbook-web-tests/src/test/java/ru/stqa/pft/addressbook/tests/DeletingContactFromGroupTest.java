eb366418c975ea6776c5d673f96cc82888168334
Serg <polwen@yandex.ru>
2021-07-11 22:01:01 +0300

    Add deleting test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeletingContactFromGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeletingContactFromGroupTest.java
@@ -1,110 +1,50 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.GroupSet;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-/**
- * Предулосвие
- * 1. Создать группу
- * тест
- * 2. Добавить в эту группу контакт
- * 3. Зафиксировать контакт
- * 4. Удалить контакт
- * 5. Проверить
- *
- * Постусловие
- * Удалить группу
- */
 public class DeletingContactFromGroupTest extends TestBase {
+
 
     @BeforeMethod
     public void ensurePreconditions() {
         if (app.db().getAllContacts().size() == 0) {
-            ContactData contact = new ContactData()
-                    .withName("contact for removing in group")
-                    .withLastName("contact for removing in group")
-                    .withEmail("addingingroup@mail.gmail")
-                    .withMobilePhone("880050000");
-            app.goTo().addNewUserPage();
-            app.contact().create(contact);
-            app.goTo().homePage();
+            createContact();
         }
 
-        app.goTo().groupPage();
-        app.group().create(new GroupData().withName("Group for adding and removing contact"));
-        app.goTo().homePage();
-
-
-
+        if (app.db().getAllGroups().size() == 0) {
+            createGroup();
+        }
     }
 
     @Test
     public void testDeletingContactFromGroup() {
-        Map<String, Object> data = addContactToGroup();
-        GroupData group = (GroupData) data.get("group");
-        ContactData randomContactToAddingInGroup = (ContactData) data.get("contact");
+        ContactData before = app.db().getAllContacts().iterator().next(); // получили произвольный контакт из БД
+        GroupSet contactGroups = before.getGroups(); // получили список его групп
 
+        if (contactGroups.size() == 0) { // если полученный контакт не состоит ни в одной в группе, то
+            GroupData groupToAdd = app.db().getAllGroups().iterator().next(); // находим группу для добавления
+            app.goTo().homePage();
+            app.contact().addContactToGroup(before.getId(), groupToAdd.getId()); // добавляем контакт в группу
 
-        ContactData currentContact = app.db().getCurrentContact(randomContactToAddingInGroup.getId());
+            ContactData beforeWithGroup = app.db().getCurrentContact(before.getId()); // находим исходный контакт в БД
+            contactGroups.addAll(beforeWithGroup.getGroups()); // добавляем к пустому списку групп исходного конткта, группу нового контакта
+            contactGroups.forEach(before::inGroup); // добавляем новую группу к нашему контакту
+        }
+
+        GroupData groupToRemove = contactGroups.iterator().next();
 
         app.goTo().homePage();
-
-        app.contact().removeContactFromGroup(group.getId(), randomContactToAddingInGroup.getId());
-
-        ContactData contactFromDb = app.db().getCurrentContact(randomContactToAddingInGroup.getId());
-
-        assertThat(contactFromDb, equalTo(
-                currentContact.outOfGroup(group)
-        ));
+        app.contact().removeContactFromGroup(groupToRemove.getId(), before.getId());
+        ContactData after = app.db().getCurrentContact(before.getId());
+        assertThat(after, equalTo(before.outOfGroup(groupToRemove)));
 
     }
 
-
-
-    @AfterMethod
-    public void ensurePostconditions() {
-        GroupSet groupsFromDb = app.db().getAllGroups();
-        GroupData deletedGroup = app.db().getCurrentGroup(
-                groupsFromDb.stream()
-                        .mapToInt(GroupData::getId)
-                        .max().getAsInt()
-        );
-        app.goTo().groupPage();
-        app.group().delete(deletedGroup);
-    }
-
-
-    /**
-     * Метод чтобы добавить в группу контакт для дальнешего удаления
-     * @return
-     * возвращает таблицу с данными о группе и контакте чтобы в дальнейшем пользоваться ими в тесте
-     */
-    public Map<String, Object> addContactToGroup() {
-        ContactData randomContactToAddingInGroup = app.db().getAllContacts().iterator().next();
-        GroupSet groupsFromDb = app.db().getAllGroups();
-        GroupData group = app.db().getCurrentGroup(
-                groupsFromDb.stream()
-                        .mapToInt(GroupData::getId)
-                        .max().getAsInt()
-        );
-        app.goTo().homePage();
-        app.contact().addContactToGroup(randomContactToAddingInGroup.getId(), group.getId());
-
-
-        Map<String, Object> data = new HashMap<>();
-        data.put("contact", randomContactToAddingInGroup);
-        data.put("group", group);
-        return data;
-    }
 }
