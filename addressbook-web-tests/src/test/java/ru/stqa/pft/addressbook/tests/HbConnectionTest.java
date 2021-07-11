e84252ac1d1d4b21c4e5458600f84004a6d1f505
Serg <polwen@yandex.ru>
2021-07-11 17:12:12 +0300

    Change HbConnection

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -37,7 +37,6 @@ public class HbConnectionTest {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         List<ContactData> result = session.createQuery("from ContactData where deprecated = '0000-00-00 00:00:00'").list();
-
         session.getTransaction().commit();
         session.close();
         for (ContactData contact : result) {
