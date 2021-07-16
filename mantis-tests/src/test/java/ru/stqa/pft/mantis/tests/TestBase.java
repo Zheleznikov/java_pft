a7cd10faeb953b6665170d3b06c2d8379602a731
Serg <polwen@yandex.ru>
2021-07-16 16:12:44 +0300

    Add new module for testing mantis project

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    //    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method method, Object[] parameters) {
+        logger.info("Start test - " + method.getName() + " with parameters " + Arrays.asList(parameters));
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void togTestStop(Method method) {
+        logger.info("Stop test - " + method.getName());
+
+    }
+
+
+}
