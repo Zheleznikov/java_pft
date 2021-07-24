498b9ffee3a473b6a76dc2476d7dc309717d510c
Serg <polwen@yandex.ru>
2021-07-24 14:18:03 +0300

    Add bugifyTest - add issue

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -0,0 +1,59 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+
+
+public class RestTests {
+
+    private final String HOST = "https://bugify.stqa.ru/api/";
+    private final String API_KEY = "288f44776e7bec4bf44fdfeb1e646490";
+
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue()
+                .withSubject("Test issue")
+                .withDescription("New test issue");
+        int newIssueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(newIssueId));
+        assertEquals(newIssues, oldIssues);
+    }
+
+
+    private Set<Issue> getIssues() throws IOException {
+        String json = getExecutor().execute(Request.Get(HOST + "issues.json"))
+                .returnContent().asString();
+//        JsonObject parsed = JsonParser.parseString(json).getAsJsonObject();
+        JsonElement issues = JsonParser.parseString(json).getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
+    }
+
+    private int createIssue(Issue newIssue) throws IOException {
+        String json = getExecutor().execute(Request.Post(HOST + "issues.json")
+                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                        new BasicNameValuePair("description", newIssue.getDescription())))
+                .returnContent().asString();
+        return JsonParser.parseString(json).getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+    private Executor getExecutor() {
+        return Executor.newInstance().auth(API_KEY, "");
+    }
+
+}
